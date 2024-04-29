121eac782388e1ec282062c50b1254dcbf3df565
max cha <max@nizcorp.kr>
2024-04-29 16:43:08 +0900

    fix : format

--- examples/recognition_simple/recognition_simple.ino
+++ examples/recognition_simple/recognition_simple.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+
 #include <Mecha_VoiceRecognition.h>
 
 #define PIN_LED_GREEN 4
@@ -15,7 +16,7 @@ void setup() {
   pinMode(PIN_LED_RED, OUTPUT);
 
   Serial.begin(9600);
-  
+
   voice.setDebugOn(&Serial);
   voice.setGroup(VOICE_GROUP_1);
 
@@ -34,42 +35,42 @@ void setup() {
 
 void loop() {
   VOICE_InstructionTypeDef response = voice.getInstruction();
-  switch(response) {
-    case VOICE_INSTRUCTION_1:
-      Serial.println("Red");
-      digitalWrite(PIN_LED_GREEN, LOW);
-      digitalWrite(PIN_LED_YELLOW, LOW);
-      digitalWrite(PIN_LED_RED, HIGH);
-      break;
-    case VOICE_INSTRUCTION_2:
-      Serial.println("Yellow");
-      digitalWrite(PIN_LED_GREEN, LOW);
-      digitalWrite(PIN_LED_YELLOW, HIGH);
-      digitalWrite(PIN_LED_RED, LOW);
-      break;
-    case VOICE_INSTRUCTION_3:
-      Serial.println("Green");
-      digitalWrite(PIN_LED_GREEN, HIGH);
-      digitalWrite(PIN_LED_YELLOW, LOW);
-      digitalWrite(PIN_LED_RED, LOW);
-      break;
-    case VOICE_INSTRUCTION_4:
-      Serial.println("White");
-      digitalWrite(PIN_LED_GREEN, HIGH);
-      digitalWrite(PIN_LED_YELLOW, HIGH);
-      digitalWrite(PIN_LED_RED, HIGH);
-      break;
-    case VOICE_INSTRUCTION_5:
-      Serial.println("Black");
-      digitalWrite(PIN_LED_GREEN, LOW);
-      digitalWrite(PIN_LED_YELLOW, LOW);
-      digitalWrite(PIN_LED_RED, LOW);
-      break;
-    case VOICE_INSTRUCTION_ERROR:
-      Serial.println("error");
-      break;
-    case VOICE_INSTRUCTION_NOTHING:
-      // no data recieved
-      break;
+  switch (response) {
+  case VOICE_INSTRUCTION_1:
+    Serial.println("Red");
+    digitalWrite(PIN_LED_GREEN, LOW);
+    digitalWrite(PIN_LED_YELLOW, LOW);
+    digitalWrite(PIN_LED_RED, HIGH);
+    break;
+  case VOICE_INSTRUCTION_2:
+    Serial.println("Yellow");
+    digitalWrite(PIN_LED_GREEN, LOW);
+    digitalWrite(PIN_LED_YELLOW, HIGH);
+    digitalWrite(PIN_LED_RED, LOW);
+    break;
+  case VOICE_INSTRUCTION_3:
+    Serial.println("Green");
+    digitalWrite(PIN_LED_GREEN, HIGH);
+    digitalWrite(PIN_LED_YELLOW, LOW);
+    digitalWrite(PIN_LED_RED, LOW);
+    break;
+  case VOICE_INSTRUCTION_4:
+    Serial.println("White");
+    digitalWrite(PIN_LED_GREEN, HIGH);
+    digitalWrite(PIN_LED_YELLOW, HIGH);
+    digitalWrite(PIN_LED_RED, HIGH);
+    break;
+  case VOICE_INSTRUCTION_5:
+    Serial.println("Black");
+    digitalWrite(PIN_LED_GREEN, LOW);
+    digitalWrite(PIN_LED_YELLOW, LOW);
+    digitalWrite(PIN_LED_RED, LOW);
+    break;
+  case VOICE_INSTRUCTION_ERROR:
+    Serial.println("error");
+    break;
+  case VOICE_INSTRUCTION_NOTHING:
+    // no data recieved
+    break;
   }
 }
